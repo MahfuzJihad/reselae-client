4888abeb2b24032a8be19570e33cd0f795f7eb83
jihadmahfuz <jihadmah166@gmail.com>
2022-11-29 17:58:28 +0600

    third commit

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,44 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import React from "react";
+import { Link } from "react-router-dom";
+
 
 const SignUp = () => {
-    const handlesignUp = event =>{
-        event.preventDefault();
-     }
+  const handlesignUp = (event) => {
+    event.preventDefault();
+  };
 
-    return (
-        <div className="hero w-full">
-        <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row">
-          <div className="text-center lg:text-left">
-            
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-20 py-20">
-            <form onSubmit={handlesignUp} className="card-body">
-              <div className="form-control">
+  return (
+    <div className="hero w-full">
+      <div className="hero-content grid md:grid-cols-2 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 my-20 py-20">
+          <form onSubmit={handlesignUp} className="card-body">
+            <div className="form-control">
               <h1 className="text-5xl text-center font-bold">Sign Up!</h1>
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="text" placeholder="email" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="text" placeholder="password" className="input input-bordered" />
-                
-              </div>
-              <div className="form-control mt-6">
-                  <input className="btn btn-primary" type="submit" value="Login" />
-                
-              </div>
-            </form>
-            {/* <p className='text-center'>New to Resale website? please <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p> */}
-          </div>
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name='name'
+                placeholder="Your name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                name='email'
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                name='name'
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <input className="btn btn-primary" type="submit" value="Sign Up" />
+            </div>
+          </form>
+          <p className='text-center'>Already have an account? please <Link className='text-orange-600 font-bold' to="/login">Login</Link> </p>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default SignUp;
